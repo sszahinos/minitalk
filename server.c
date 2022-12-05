e001eb78fc5cbdea94bb966eb6e48a2078fc4dc8
Sergio Sanchez Zahinos <sersanch@student.42barcelona.com>
2022-12-05 14:23:22 +0100

    update

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: sersanch <sersanch@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 09:17:08 by sersanch          #+#    #+#             */
-/*   Updated: 2022/12/05 10:27:31 by sersanch         ###   ########.fr       */
+/*   Updated: 2022/12/05 14:23:09 by sersanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 #include <unistd.h>
 #include <stdio.h> //borrar
 
+void	singal_handler(int signal, siginfo_t *info, void *context)
+{
+	if (info->si_pid != 0)
+		c_pid = info->si_pid;
+}
+
 // Max PID number 99999 (32 bits)
 void	print_bit(int sig_num)
 {
@@ -22,24 +28,25 @@ void	print_bit(int sig_num)
 	//exit(1);
 }
 
-void	start_server(void)
+void	start_server(sigaction act)
 {
-	pid_t	pid;
+	pid_t	s_pid;
 
-	pid = getpid();
-	signal(SIGUSR1, print_bit);
+	s_pid = getpid();
+	sigaction(SIGUSR1, act, NULL);
 	while (1)
 	{
-		printf("PID: %d Waiting for signal...\n", (int)pid);
+		printf("PID: %d Waiting for signal...\n", (int)s_pid);
 		sleep(10);
 	}
 }
 
-int	main(int argc, char** argv)
+int	main(void)
 {
-	argc = 1;
-	if (argv)
-		;
-	start_server();
+	struct sigaction	act;
+	act.sa_flags = SA_SIGINFO;
+    sigemptyset(&act.sa_mask);
+    act.sa_sigaction = signal_handler;
+	start_server(act);
 	return (0);
 }
