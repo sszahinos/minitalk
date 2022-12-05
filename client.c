d8e2720d3bc1122ea44046789419bb4a879440fd
Sergio Sanchez Zahinos <sersanch@student.42barcelona.com>
2022-12-05 12:11:59 +0100

    client

--- client.c
+++ client.c
@@ -6,35 +6,50 @@
 /*   By: sersanch <sersanch@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 15:07:21 by sersanch          #+#    #+#             */
-/*   Updated: 2022/12/01 12:23:08 by sersanch         ###   ########.fr       */
+/*   Updated: 2022/12/05 12:11:41 by sersanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <stdio.h> //borrar
 
-/*void	send_bit(char c)
+void	send_num(int pid, int num, int n_bits)
 {
-	kill();
-}*/
+	int	*bin_num;
+	int i;
 
-void	send_message(int pid, char* message)
+	bin_num = ft_itoa_bin(num, n_bits);
+	i = 0;
+	while (i < n_bits)
+	{
+		if (bin_num[i] == 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i++;
+		pause();
+	}
+}
+
+void	send_message(int pid, char *message)
 {
 	int	i;
-	//struct sigaction	act;
 
 	i = 0;
 	while (message[i])
 	{
-		send_bit(message[i]);
+		send_num((int)message[i]);
 		i++;
 	}
-	send_bit('\0');
+	send_num((int)'\0');
 }
 
-int	main(int argc, char** argv)
+int	main(int argc, char **argv)
 {
 	if (argc == 3)
+	{
+		send_num((int)argv[1], (int)getpid(), 24);
 		send_message((int)argv[1], argv[2]);
+	}
 	return (0);
 }
