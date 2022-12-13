fcc4d12532a9b89efc9d4c0cb28ce27b5057bddb
Sergio Sanchez Zahinos <sersanch@student.42barcelona.com>
2022-12-13 10:50:49 +0100

    update

--- client.c
+++ client.c
@@ -6,13 +6,14 @@
 /*   By: sersanch <sersanch@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 15:07:21 by sersanch          #+#    #+#             */
-/*   Updated: 2022/12/05 14:23:12 by sersanch         ###   ########.fr       */
+/*   Updated: 2022/12/13 10:47:47 by sersanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <signal.h>
 #include <stdio.h> //borrar
-
+/*
 void	send_num(int pid, int num, int n_bits)
 {
 	int	*bin_num;
@@ -42,14 +43,40 @@ void	send_message(int pid, char *message)
 		i++;
 	}
 	send_num((int)'\0');
+}*/
+
+void	send_pid(s_pid)
+{
+	printf("Sending PID\n");
+	kill(s_pid, SIGUSR1);
+	//kill(s_pid, SIGUSR2);
+}
+
+void	signal_handler(int signal, siginfo_t *info, void *context)
+{
+	printf("Llega mensaje de server.\n");
 }
 
 int	main(int argc, char **argv)
 {
-	if (argc == 3)
+	struct sigaction	act;
+
+	act.sa_flags = SA_SIGINFO;
+	sigemptyset(&act.sa_mask);
+	act.sa_sigaction = signal_handler;
+/*	if (argc == 3)
 	{
-		send_num((int)argv[1], (int)getpid(), 24);
-		send_message((int)argv[1], argv[2]);
+		send_pid(8063);
+//		send_num((int)argv[1], (int)getpid(), 24);
+//		send_message((int)argv[1], argv[2]);
+	}*/
+	int c_pid = getpid();
+	while (1)
+	{
+		printf("Client PID> %d\n", (int)c_pid);
+		send_pid(8362);
+		sleep(10);
 	}
+
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: sersanch <sersanch@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 09:17:08 by sersanch          #+#    #+#             */
-/*   Updated: 2022/12/05 14:23:09 by sersanch         ###   ########.fr       */
+/*   Updated: 2022/12/13 10:46:49 by sersanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,14 @@
 #include <unistd.h>
 #include <stdio.h> //borrar
 
-void	singal_handler(int signal, siginfo_t *info, void *context)
+void	signal_handler(int signal, siginfo_t *info, void *context)
 {
+	int	c_pid;
+
 	if (info->si_pid != 0)
 		c_pid = info->si_pid;
+	printf("pid> %d\n", c_pid);
+	kill(c_pid, SIGUSR1);
 }
 
 // Max PID number 99999 (32 bits)
@@ -28,22 +32,31 @@ void	print_bit(int sig_num)
 	//exit(1);
 }
 
-void	start_server(sigaction act)
+void	start_server(struct sigaction act)
 {
 	pid_t	s_pid;
 
 	s_pid = getpid();
-	sigaction(SIGUSR1, act, NULL);
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
 	while (1)
 	{
 		printf("PID: %d Waiting for signal...\n", (int)s_pid);
 		sleep(10);
 	}
 }
-
+/*
+ * sigaction permite personalizar una senyal.
+ * Utilizando la flag SA_SIGINFO, permite detectar informacion adicional sobre
+ * la senyal.
+ * sa_mask hace que las senyales anyadidas sean bloqueadas. Como no nos
+ * interesa ninguna en especial, la creamos vacia con sigemptyset.
+ * sa_sigaction -> nos permite ejecutar una funcion cuando se reciba la senyal
+ */
 int	main(void)
 {
 	struct sigaction	act;
+
 	act.sa_flags = SA_SIGINFO;
     sigemptyset(&act.sa_mask);
     act.sa_sigaction = signal_handler;
